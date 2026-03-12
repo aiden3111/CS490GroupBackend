81f482d6a7ad16519c5e19bd25267d38376b865b
Carlos <clc52@njit.edu>
2026-03-12 19:03:11 -0400

    Added dependencies and fixed typos

--- setup.bat
+++ setup.bat
@@ -6,16 +6,17 @@ echo Activating virtual environment...
 call venv\Scripts\activate
 
 echo Installing dependencies...
-py -m pip install flask flask-cors google-auth-oauthlib google-auth
+py -m pip install flask flask-cors google-auth-oauthlib google-auth python-dotenv mysql-connector-python
 
 echo.
 echo Setup complete! To start developing, activate the venv with:
-echo    source venv\Scripts\activate
-echo	Please create your own .env file as well with the following information:
-echo	DB_HOST=<Your Database Host>
-echo	DB_USER=<Your Database Username>
-echo 	DB_PASSWORD=<Your Database Password>
-echo	DB_NAME=<Your Database Name>
-echo	GOOGLE_CLIENT_ID=<our client id>
-echo 	GOOGLE_CLIENT_SECRET=<our client secret>
+echo source venv/Scripts/activate
+echo Please create your own .env file as well with the following information:
+echo    DB_HOST=^<Your Database Host^>
+echo    DB_USER=^<Your Database Username^>
+echo    DB_PASSWORD=^<Your Database Password^>
+echo    DB_NAME=^<Your Database Name^>
+echo    GOOGLE_CLIENT_ID=^<your client id^>
+echo    GOOGLE_CLIENT_SECRET=^<your client secret^>
+
 pause
