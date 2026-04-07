f26751c841a1b1cfcb484f83b1b7c88cc42e6e40
Aiden <kicotan2004@gmail.com>
2026-04-07 15:44:12 -0400

    Use case 8.4 deleting mood log entry

--- routes/mood.py
+++ routes/mood.py
@@ -12,13 +12,14 @@ def get_my_mood(client_id):
     try:
         cursor.execute("""
             SELECT
+                mood_log_id, 
                 log_date,
                 mood_score,
                 mood_label,
                 notes
             FROM mood_log
             WHERE client_id = %s
-        """, (client_id,))
+        """, (client_id,)) # added mood_log_id since thats in the DB but wasnt in the query -- Aiden
         
         moodlog = cursor.fetchall()
 
@@ -35,3 +36,27 @@ def get_my_mood(client_id):
         conn.close()
 
 
+@mood_bp.route("/<int:log_id>", methods=["DELETE"]) # Add deleting mood logs -- Aiden
+def delete_mood_log(log_id):
+    conn = get_conn()
+    cursor = conn.cursor(dictionary=True)
+
+    try:
+        cursor.execute("""
+            DELETE FROM mood_log
+            WHERE mood_log_id = %s
+        """, (log_id,))
+
+        conn.commit()
+
+        if cursor.rowcount == 0:
+            return jsonify({"error": "Log not found"}), 404
+        
+        return jsonify({"message": "Log successfuly deleted"}), 200
+    
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+    
+    finally: 
+        cursor.close()
+        conn.close()
