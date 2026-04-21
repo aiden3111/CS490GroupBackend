ffb064f34724ce92299074f526c80664f9e9115f
Aiden <kicotan2004@gmail.com>
2026-04-21 17:06:02 -0400

    added invoices to db with a query

--- databaseStuff/db_updates2.sql
+++ databaseStuff/db_updates2.sql
@@ -11,3 +11,13 @@ CREATE TABLE IF NOT EXISTS payment_method (
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (client_id) REFERENCES client(client_id)
 );
+
+-- uc 13.2 monthly invoices
+CREATE TABLE IF NOT EXISTS invoice(
+    invoice_id INT AUTO_INCREMENT PRIMARY KEY,
+    client_id VARCHAR(50) NOT NULL,
+    amount DECIMAL(8,2),
+    billing_month VARCHAR(20),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (client_id) REFERENCES client(client_id)
+);
